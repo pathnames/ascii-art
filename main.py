6f561670b667f585e2f7be31ccf08cf8db389854
pjkpjk900 <pk58@buffalo.edu>
2020-12-29 16:29:25 -0500

    2d matrix achieved

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PIL import Image
+
+
 img = Image.open("ascii-pineapple.jpg")
 
 #https://en.wikibooks.org/wiki/Python_Imaging_Library/Editing_Pixels
@@ -7,10 +9,7 @@ img = Image.open("ascii-pineapple.jpg")
 #     for j in range(img.size[1]): #for every row
 #         pixel_array.append(img[i][j])
 
-pixel_array = list(img.getdata())
-pixel_matrix = []
-for i in range(img.size[0]): #for every column
-    for j in range(img.size[1]): #for every row
-        pixel_matrix.append(pixel_array[i][j])
+pixels = list(img.getdata())
+pixel_matrix = [pixels[i:i+img.width] for i in range(0, len(pixels), img.width)]
 
 print(pixel_matrix)
