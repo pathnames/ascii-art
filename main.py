41bfe62f71a4a2f00016ef02ab7aff2031cb0ec0
pjkpjk900 <pk58@buffalo.edu>
2020-12-29 21:45:27 -0500

    Brightness matrix done!

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ from PIL import Image
 
 img = Image.open("ascii-pineapple.jpg")
 
-#https://en.wikibooks.org/wiki/Python_Imaging_Library/Editing_Pixels
-# pixel_array = []
-# for i in range(img.size[0]): #for every column
-#     for j in range(img.size[1]): #for every row
-#         pixel_array.append(img[i][j])
-
+#Convert image to a 2d-matrix:
 pixels = list(img.getdata())
 pixel_matrix = [pixels[i:i+img.width] for i in range(0, len(pixels), img.width)]
 
-print(pixel_matrix)
+brightness_matrix = []
+for x in range(len(pixel_matrix)):
+    list = []
+    for y in range(len(pixel_matrix)):
+        list.append((pixel_matrix[x][y][0] + pixel_matrix[x][y][1] + pixel_matrix[x][y][2])/3)
+    brightness_matrix.append(list)
+
+def print_matrix(matrix):
+    for x in range(len(matrix)):
+        for y in range(len(matrix)):
+            print(matrix[x][y])
+        
+print(len(brightness_matrix))
+#print_matrix(brightness_matrix)
+
+
+
+
